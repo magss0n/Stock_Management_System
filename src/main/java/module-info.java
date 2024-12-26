158a3990353f6a2fdef07685b66c9cd74e831858
M-S-G-A <moutcheuanderson@gmail.com>
2024-12-26 11:29:22 +0100

    config files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.gui {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
     opens com.example.gui to javafx.fxml;
     exports com.example.gui;
