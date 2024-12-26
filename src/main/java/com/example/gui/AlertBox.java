5d71363e2f34dd9a67b12ed46b651ea5649f5923
M-S-G-A <moutcheuanderson@gmail.com>
2024-12-26 11:17:54 +0100

    Popup for Error or Confirmation Messages

---
+++ src/main/java/com/example/gui/AlertBox.java
@@ -0,0 +1,37 @@
+package com.example.gui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AlertBox {
+    public static void display(String title, String message){
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title + "!!");
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(event -> window.close());
+
+        VBox vbox = new VBox(20);
+        vbox.setMinSize(400, 100);
+        vbox.getChildren().addAll(label, closeButton);
+        vbox.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(vbox);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}
