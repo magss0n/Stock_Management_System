bd391ef62e21ed547a1441a18a17a89214702442
M-S-G-A <moutcheuanderson@gmail.com>
2024-12-26 11:25:35 +0100

    Stock movement or stock transaction. Represents all stock entries and exits made.

---
+++ src/main/java/com/example/gui/StockMovement.java
@@ -0,0 +1,23 @@
+package com.example.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockMovement {
+    public List<StockEntry> stockPurchases = new ArrayList<>();
+    public List<StockExit> stockSales = new ArrayList<>();
+
+
+
+
+
+    public void viewPurchaseReport(){
+
+    }
+
+    public void viewSalesReports(){
+
+    }
+
+
+}
