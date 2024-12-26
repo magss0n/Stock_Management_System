b04bf30ed9b99d8601e6eb84f51cf74109196035
M-S-G-A <moutcheuanderson@gmail.com>
2024-12-26 11:26:21 +0100

    Supplier class. Represents a supplier of a given product.

---
+++ src/main/java/com/example/gui/Supplier.java
@@ -0,0 +1,20 @@
+package com.example.gui;
+
+public class Supplier extends Person{
+    private String contributor_id;
+    public String location;
+    public Supplier(String name, String contact, String location){
+        super(name, contact);
+        contributor_id = "QWNSF-838UEJ";
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "location='" + location + '\'' +
+                ", name='" + name + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
+}
