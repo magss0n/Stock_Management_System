1e45a2b798ca88cade8419a46ca005cc3944c191
M-S-G-A <moutcheuanderson@gmail.com>
2025-01-16 00:21:22 +0100

    Date attribute added

--- src/main/java/com/example/gui/StockExit.java
+++ src/main/java/com/example/gui/StockExit.java
@@ -1,16 +1,20 @@
 package com.example.gui;
 
+import java.util.Date;
+
 public class StockExit{
     Customer customer;
     Double salesAmt;
     Product product;
     Integer qtySold;
+    Date date;
 
     public StockExit(Customer c, Product p, Double amt, Integer qty){
         customer = c;
         salesAmt = amt;
         product = p;
         qtySold = qty;
+        date = new Date();
     }
 
     public void printPaymentReceipt(){
