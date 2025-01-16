f966220cb12dc1452b07853dbe03b3e6b0276ccc
MANUEL WILFRID <manuelwilfrid123@gmail.com>
2025-01-16 18:39:30 +0100

    Update Main.java

--- src/main/java/com/example/gui/Main.java
+++ src/main/java/com/example/gui/Main.java
@@ -30,8 +30,6 @@ public class Main extends Application {
     static Product prodSelected;
     static Stock stock = new Stock();
     static StockMovement stockMovement = new StockMovement();
-    static Integer attempts = 0;
-
 
 
     @Override
@@ -58,18 +56,9 @@ public class Main extends Application {
         button1.setOnAction(event -> {
             if (!admin.authenticate(idField.getText(),passwordField.getText())) {
                 AlertBox.display("Error!", "Invalid user Id or Password");
-                attempts += 1;
-                if (attempts == 2){
-                    AlertBox.display("Caution", "You have 1 attempt left");
-                }
-                if (attempts == 3){
-                    AlertBox.display("Shutting Down", "Too many failed login attempts");
-                    window.close();
-                }
             }
             else {
                 mainMenu();
-
             }
         });
         Button button2 = new Button("Clear");
@@ -140,13 +129,20 @@ public class Main extends Application {
         Button b4 = new Button("View Customers/Suppliers");
         b4.setOnAction(event -> viewCustomersOrSuppliers());
 
-        Button b5 = new Button("Exit");
-        b5.setStyle("-fx-color: RED;");
+        Button b5 = new Button("View Purchase Report");
         b5.setOnAction(event -> {
+            PurchaseReport report = new PurchaseReport(stockMovement);
+            report.display();
+        });// qdded b5 b6 for purchase and sales reports
+
+
+        Button b7 = new Button("Exit");
+        b7.setStyle("-fx-color: RED;");
+        b7.setOnAction(event -> {
             if(AlertBox.confirm("Exit")) window.close();
         });
 
-        layout.getChildren().addAll(welcome,b1,b2,b3,b4,b5);
+        layout.getChildren().addAll(welcome,b1,b2,b3,b4,b5,b7); //added all buttons
         layout.setAlignment(Pos.CENTER);
         scene2 = new Scene(layout);
 
@@ -233,23 +229,13 @@ public class Main extends Application {
             }
 
             if (suppliers.contains(supSelected) && stock.getProducts().contains(prodSelected) && !qty.getText().isEmpty() && !amount.getText().isEmpty()){
-                try{
-                    Integer quantity = Integer.parseInt(qty.getText());
-                    Double amt = Double.parseDouble(amount.getText());
-                    if(amt/quantity > prodSelected.getCostPrice()){
-                        AlertBox.display("Error", "Purchase amount and Quantity do not match Cost Price");
-                    }
-                    else{
-                        stock.setQty(prodSelected, quantity);
-                        stockMovement.stockPurchases.add(new StockEntry(supSelected,amt, prodSelected, quantity));
-                        System.out.println("Entries" + stockMovement.stockPurchases);
-                        AlertBox.display("Successful", "Stock Entry Successfully saved");
-                        window.setScene(scene2);
-                    }
-
-                } catch (Exception e) {
-                    AlertBox.display("Error", "Quantity and Purchase amount should be numbers");
-                }
+                Integer quantity = Integer.parseInt(qty.getText());
+                Double amt = Double.parseDouble(amount.getText());
+                stock.setQty(prodSelected, quantity);
+                stockMovement.stockPurchases.add(new StockEntry(supSelected,amt, prodSelected, quantity));
+                System.out.println("Entries" + stockMovement.stockPurchases);
+                AlertBox.display("Successful", "Stock Entry Successfully saved");
+                window.setScene(scene2);
             }
             else AlertBox.display("Error", "One of the  required fields is empty");
         });
@@ -334,23 +320,18 @@ public class Main extends Application {
             }
 
             if (customers.contains(custSelected) && stock.getProducts().contains(prodSelected) && !qty.getText().isEmpty() && !amount.getText().isEmpty()){
-                try {
-                    Integer quantity = Integer.parseInt(qty.getText());
-                    Double amt = Double.parseDouble(amount.getText());
+                Integer quantity = Integer.parseInt(qty.getText());
+                Double amt = Double.parseDouble(amount.getText());
 
-                    if (stock.editProductQty(prodSelected, quantity)){
-                        stockMovement.stockSales.add(new StockExit(custSelected, prodSelected,amt, quantity));
-                        System.out.println("Sales: " + stockMovement.stockSales);
-                        AlertBox.display("Successful", "Stock Exit Successfully saved");
-                        window.setScene(scene2);
-
-                    }
-
-                } catch (Exception e) {
-                    AlertBox.display("Error","Quantity and Sales amount should be numbers");
+                if (stock.editProductQty(prodSelected, quantity)){
+                    stockMovement.stockSales.add(new StockExit(custSelected, prodSelected,amt, quantity));
+                    System.out.println("Sales: " + stockMovement.stockSales);
+                    AlertBox.display("Successful", "Stock Exit Successfully saved");
                 }
 
+                window.setScene(scene2);
             }
+
             else AlertBox.display("Error", "One of the  required fields is empty");
         });
 
