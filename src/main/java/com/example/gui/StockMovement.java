27056e2dcead02b4fcd1c960b5c29b5b47f774c2
MANUEL WILFRID <manuelwilfrid123@gmail.com>
2025-01-16 18:42:17 +0100

    Update StockMovement.java

--- src/main/java/com/example/gui/StockMovement.java
+++ src/main/java/com/example/gui/StockMovement.java
@@ -7,15 +7,23 @@ public class StockMovement {
     public List<StockEntry> stockPurchases = new ArrayList<>();
     public List<StockExit> stockSales = new ArrayList<>();
 
+public  void addSale(StockExit sale){
+    stockSales.add(sale);
+}
 
-
+public void addPurchase(StockEntry purchase){
+    stockPurchases.add(purchase);
+}
 
 
     public void viewPurchaseReport(){
+        PurchaseReport report = new PurchaseReport(this);
+        report.display();
 
     }
 
     public void viewSalesReports(){
+
 
     }
 
